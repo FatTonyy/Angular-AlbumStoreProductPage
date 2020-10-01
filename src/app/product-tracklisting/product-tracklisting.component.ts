3fc6dd1fa47c2cc147bf9ec741d0ea319e7b2cb0
f.X Blanson <fat_tonyy@icloud.com>
2020-10-01 15:29:21 +0000

    fixes

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -8,12 +8,12 @@ import { ProductService } from "../product.service";
   styleUrls: ["./product-tracklisting.component.css"],
 })
 export class ProductTracklistingComponent implements OnInit {
-  AlbumInfo: Album;
+  albumInfo: Album;
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
     this._productService
       .getAlbum(1)
-      .subscribe((Response) => (this.AlbumInfo = Response));
+      .subscribe((response) => (this.albumInfo = response));
   }
 }
